af585348f40efe7f21d27a250466d8400036e2d8
Akhand Pratap Singh <akhandpratapsingh@gmail.com>
2025-01-23 19:14:06 +0530

    Create Project

---
+++ Protfolio_Icon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Protfolio_Icon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
