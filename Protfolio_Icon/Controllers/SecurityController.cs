ce51a302b6ae94d06f8f246c88183388a8df9a0c
Akhand Pratap Singh <akhandpratapsingh@gmail.com>
2025-01-27 19:16:07 +0530

    Ingnored fix

---
+++ Protfolio_Icon/Controllers/SecurityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Protfolio_Icon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Protfolio_Icon.Controllers
+{
+    public class SecurityController : Controller
+    {
+        private const string LOGIN_ROUTENAME = "apslogin";
+
+        [HttpGet]
+        [Route(LOGIN_ROUTENAME)]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route(LOGIN_ROUTENAME)]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View();
+        }
+    }
+}
