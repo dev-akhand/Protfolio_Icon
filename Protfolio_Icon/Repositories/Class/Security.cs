543d257d9dfb22ebbc3c4275727d42d40b617a07
Akhand Pratap Singh <akhandpratapsingh@gmail.com>
2025-01-27 19:20:28 +0530

    Repo

---
+++ Protfolio_Icon/Repositories/Class/Security.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Protfolio_Icon.Repositories.Class
+{
+    public class Security
+    {
+    }
+}
